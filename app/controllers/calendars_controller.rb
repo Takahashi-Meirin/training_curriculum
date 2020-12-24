bb4566d49b18b41a892fafc0f28f2f9667f33bf1
Ramrin <73950473+Takahashi-Meirin@users.noreply.github.com>
2020-12-24 18:04:12 +0900

    Revert "Issue4~6 やり直し"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
 
   # 日付と予定のデータを取得
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:plans).permit(:date, :plan)
   end
 
   def get_week
@@ -41,8 +41,8 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
-
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wday[wday_num]}
+      
       @week_days.push(days)
     end
 
